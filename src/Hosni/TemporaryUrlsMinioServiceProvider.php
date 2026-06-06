e458f85291e6fd387254eb3ab5e51374072dcc8f
Hossein Hosni <hosni.hossein@gmail.com>
2026-06-06 12:25:04 +0330

    fix: using static clouse forwards class to wrong context

--- src/Hosni/TemporaryUrlsMinioServiceProvider.php
+++ src/Hosni/TemporaryUrlsMinioServiceProvider.php
@@ -6,41 +6,36 @@ use Illuminate\Filesystem\AwsS3V3Adapter;
 use Illuminate\Filesystem\FilesystemManager;
 use Illuminate\Filesystem\MinioS3V3Adapter;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Foundation\Application;
 
 class TemporaryUrlsMinioServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->app->extend('filesystem', self::extendFilesystem(...));
-        $this->app->extend(FilesystemManager::class, self::extendFilesystem(...));
+        $this->app->extend('filesystem', $this->extendFilesystem(...));
+        $this->app->extend(FilesystemManager::class, $this->extendFilesystem(...));
     }
 
-    public static function extendFilesystem(FilesystemManager $filesystem): FilesystemManager
+    protected function extendFilesystem(FilesystemManager $filesystem): FilesystemManager
     {
         return $filesystem->extend(
             's3',
-            static fn ($app, array $config) => self::extendFilesystemS3($filesystem, $config)
+            static function (Application $app, array $config) use ($filesystem) {
+                $adapter = $filesystem->createS3Driver($config);
+
+                if (isset($config['temporary_url']) and $adapter instanceof AwsS3V3Adapter) {
+                    /**
+                     * @var AwsS3V3Adapter
+                     */
+                    $virtualAdapter = $filesystem->createS3Driver(array_merge($config, [
+                        'endpoint' => $config['temporary_url'],
+                    ]));
+
+                    return MinioS3V3Adapter::fromAwsS3V3AdapterWithVirtualAdapter($adapter, $virtualAdapter);
+                }
+
+                return $adapter;
+            }
         );
-    }
-
-    /**
-     * @return \Illuminate\Contracts\Filesystem\Cloud
-     */
-    public static function extendFilesystemS3(FilesystemManager $filesystem, array $config)
-    {
-        $adapter = $filesystem->createS3Driver($config);
-
-        if (isset($config['temporary_url']) and $adapter instanceof AwsS3V3Adapter) {
-            /**
-             * @var AwsS3V3Adapter
-             */
-            $virtualAdapter = $filesystem->createS3Driver(array_merge($config, [
-                'endpoint' => $config['temporary_url'],
-            ]));
-
-            return MinioS3V3Adapter::fromAwsS3V3AdapterWithVirtualAdapter($adapter, $virtualAdapter);
-        }
-
-        return $adapter;
     }
 }
